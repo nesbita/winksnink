d3101f96e14ef20511e39f9e8ee15a2ebc205637
nesbita <ariananesbit@gmail.com>
2024-01-03 22:55:39 -0700

    added some images to portfolio and cards to services

--- src/Portfolio.jsx
+++ src/Portfolio.jsx
@@ -10,6 +10,85 @@ import ListSubheader from '@mui/material/ListSubheader';
 import IconButton from '@mui/material/IconButton';
 import InfoIcon from '@mui/icons-material/Info';
 
+const itemData = [
+    {
+      img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
+      title: 'Brow Mapping, Shape & Tint',
+      author: '@bkristastucchio',
+      rows: 2,
+      cols: 2,
+      featured: true,
+    },
+    {
+      img: 'hs_btw.png',
+      title: 'Hybrid Set, Brow Tint & Wax',
+      author: '@winksnink',
+    },
+    {
+      img: 'vs.png',
+      title: 'Volume Set',
+      author: '@winksnink',
+    },
+    {
+      img: 'cs_bl.png',
+      title: 'Classic Set on Baby Lashes',
+      author: '@winksnink',
+      cols: 2,
+    },
+    {
+      img: 'hs_bs.png',
+      title: 'Hybrid Set & Brow Shaping',
+      author: '@winksnink',
+      cols: 2,
+    },
+    {
+      img: 'cs_b.png',
+      title: 'Classic Set with a Pop of Blue',
+      author: '@winksnink',
+      rows: 2,
+      cols: 2,
+      featured: true,
+    },
+    {
+      img: 'obt_1',
+      title: 'Ombre Brow Tattoo I',
+      author: '@winksnink',
+    },
+    {
+      img: 'obt_2',
+      title: 'Ombre Brow Tattoo II',
+      author: '@winksnink',
+    },
+    // {
+    //   img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
+    //   title: 'Lash Lift & Tint',
+    //   author: '@silverdalex',
+    //   rows: 2,
+    //   cols: 2,
+    // },
+    // {
+    //   img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
+    //   title: 'Short Volume Set',
+    //   author: '@nolanissac',
+    // },
+    // {
+    //   img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
+    //   title: 'Volume Fill',
+    //   author: '@helloimnik',
+    // },
+    // {
+    //   img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
+    //   title: 'Lash Lift & Tint',
+    //   author: '@tjdragotta',
+    // },
+    // {
+    //   img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
+    //   title: 'Classic Set with D Curl',
+    //   author: '@shelleypauls',
+    // },
+    
+  ];
+
 class Portfolio extends Component {
     render() {
         return (
@@ -26,9 +105,9 @@ class Portfolio extends Component {
           </Nav>
         </Container>
       </Navbar>
-      <br />
-   
-    <h2>Take a look at some of the work I've done!</h2>
+    
+   <h1>WINKS & INK</h1>
+    {/* <h2>Take a look at some of the work I've done!</h2> */}
 {/* 
 function TitlebarImageList() { */}
 {/* //   return ( */}
@@ -62,79 +141,8 @@ function TitlebarImageList() { */}
 {/* //   );
 } */}
 
-const itemData = [
-  {
-    img: 'https://images.unsplash.com/photo-1551963831-b3b1ca40c98e',
-    title: 'Breakfast',
-    author: '@bkristastucchio',
-    rows: 2,
-    cols: 2,
-    featured: true,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1551782450-a2132b4ba21d',
-    title: 'Burger',
-    author: '@rollelflex_graphy726',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1522770179533-24471fcdba45',
-    title: 'Camera',
-    author: '@helloimnik',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1444418776041-9c7e33cc5a9c',
-    title: 'Coffee',
-    author: '@nolanissac',
-    cols: 2,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1533827432537-70133748f5c8',
-    title: 'Hats',
-    author: '@hjrc33',
-    cols: 2,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1558642452-9d2a7deb7f62',
-    title: 'Honey',
-    author: '@arwinneil',
-    rows: 2,
-    cols: 2,
-    featured: true,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1516802273409-68526ee1bdd6',
-    title: 'Basketball',
-    author: '@tjdragotta',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1518756131217-31eb79b20e8f',
-    title: 'Fern',
-    author: '@katie_wasserman',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1597645587822-e99fa5d45d25',
-    title: 'Mushrooms',
-    author: '@silverdalex',
-    rows: 2,
-    cols: 2,
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1567306301408-9b74779a11af',
-    title: 'Tomato basil',
-    author: '@shelleypauls',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1471357674240-e1a485acb3e1',
-    title: 'Sea star',
-    author: '@peterlaster',
-  },
-  {
-    img: 'https://images.unsplash.com/photo-1589118949245-7d38baf380d6',
-    title: 'Bike',
-    author: '@southside_customs',
-    cols: 2,
-  },
-];
+
+
 </>
         )
     }
--- src/Services.jsx
+++ src/Services.jsx
@@ -2,6 +2,15 @@ import React, { Component } from 'react'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
+import styled from 'styled-components'
+// import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+// import {Instagram} from '@styled-icons/boxicons-logos/Instagram'
 
 export default class Services extends Component {
     render() {
@@ -16,54 +25,392 @@ export default class Services extends Component {
             <Nav.Link href="/Portfolio">Portfolio</Nav.Link>
             <Nav.Link href="/Contact">Contact</Nav.Link>
             <Nav.Link href="/Book">Book</Nav.Link>
+            {/* <Instagram/> */}
           </Nav>
         </Container>
       </Navbar>
+
       <br />
-                LASH SERVICES:
 
-                FULL SETS
+<h1>Lash Services</h1>
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Classic Lash Extensions Full Set <br/>
+          ($25 Deposit Required Upon Booking for New Clients)
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Classic - $110 | 120 minutes
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Book</Button>
+      </CardActions>
+    </Card>
 
-                Classic Lash Extensions Full Set $110, 120 minutes, $25 deposit required upon booking for new clients.
+    <br/>
 
-                Hybrid Lash Extensions Full Set $125, 120 minutes, $25 deposit required upon booking for new clients
-                
-                Volume Lash Extension Full Set $150, 150 minutes, $25 deposit
+      <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          One Week Classic Touch Up <br/>
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          $35 | 60 minutes
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Book</Button>
+      </CardActions>
+    </Card>
 
-            
+    <br/>
 
-                FILLS 
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
 
-                Classic Fill 2 Weeks $50, 90 minutes
+      <br/>
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Classic Fill 2 Weeks
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          $50 | 90 minutes
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Book</Button>
+      </CardActions>
+    </Card>
 
-                Classic Fill 3 Weeks $60, 90 minutes
+    <br/>
 
-                Hybrid Fill 2 Weeks $60, 90 minutes
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Classic Fill 3 Weeks
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          $60 | 90 minutes
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Book</Button>
+      </CardActions>
+    </Card>
 
-                Hybrid Fill 3 Weeks $70, 90 minutes
+    <br/>
 
-                Volume Lash Fill 2 Weeks $70, 105 minutes
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Hybrid Lash Extensions Full Set <br/>
+          ($25 Deposit Required Upon Booking for New Clients)
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Hybrid - $125 | 120 minutes
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Book</Button>
+      </CardActions>
+    </Card>
 
-                Volume Lash Fill 3 Weeks $80, 90 minutes
+    <br/>
 
-                OTHER:
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          One Week Hybrid Touch Up <br/>
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          $40 | 60 minutes
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Book</Button>
+      </CardActions>
+    </Card>
 
-                One Week Touch Up $35+, 60 minutes
+    <br/>
 
-                Lash Removal $10+, 60 minutes
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Hybrid Fill 2 Weeks <br/>
+          (Fill for lash extensions - price increases per week since last fill/full set.  NO FILLS AFTER 4 WEEKS - new set is required)
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          $60 | 90 minutes
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Book</Button>
+      </CardActions>
+    </Card>
 
+<br/>
 
+<Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Hybrid Fill 3 Weeks <br/>
+          (Fill for lash extensions - price increases per week since last fill/full set.  NO FILLS AFTER 4 WEEKS - new set is required)
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          $70 | 90 minutes
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Book</Button>
+      </CardActions>
+    </Card>
 
-                BROW SERVICES:
+    <br/>
 
-                Eyebrow Shaping with Tint $25, 60 minutes, $5 deposit required for new clients
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Volume Lash Extensions Full Set <br/>
+          ($25 Deposit Required Upon Booking for New Clients)
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Hybrid - $150 | 150 minutes
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Book</Button>
+      </CardActions>
+    </Card>
 
-                Brow Shaping (NO TINT) $15, 30 minutes
-                
+    <br/>
 
-                PERMANENT MAKEUP:
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          One Week Volume Touch Up <br/>
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          $40 | 60 minutes
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Book</Button>
+      </CardActions>
+    </Card>
 
-                Annual Brow Touch Up / Color Boost $100+, 180 minutes
+    <br/>
+    
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Volume Fill 2 Weeks <br/>
+          (Fill for lash extensions - price increases per week since last fill/full set.  NO FILLS AFTER 4 WEEKS - new set is required)
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          $70 | 105 minutes
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Book</Button>
+      </CardActions>
+    </Card>
+
+      <br/>
+
+      <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Volume Fill 3 Weeks <br/>
+          (Fill for lash extensions - price increases per week since last fill/full set.  NO FILLS AFTER 4 WEEKS - new set is required)
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          $80 | 90 minutes
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Book</Button>
+      </CardActions>
+    </Card>
+
+    <br/>
+
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Lash Removal
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          $10+ | 60 minutes
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Book</Button>
+      </CardActions>
+    </Card>
+    
+    <br/>
+
+<h1>Brow Services</h1>
+
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Eyebrow Shaping with Tint <br/>
+          ($5 Deposit Required Upon Booking for New Clients)
+          Eyebrow waxing or tweezing with tint if desired.
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          Eyebrow Shaping $15 | 60 minutes
+          Shaping and Tint $25 | 60 minutes
+          Tint Only $15 | 60 minutes
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Book</Button>
+      </CardActions>
+    </Card>
+
+<br/>
+
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Eyebrow Shaping (No Tint) 
+          <br/>
+          Eyebrow waxing or tweezing with tint if desired.
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          $15 | 30 minutes
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Book</Button>
+      </CardActions>
+    </Card>
+
+<br/>
+
+<h1>Permanent Makeup</h1>
+<Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Ombre/Powder Brow Tattoo
+          ($50 Deposit Required Upon Booking)
+          **CONSULTATION IS REQUIRED**
+          This can be done virtually or in person.  Please reach out to Amanda for consultation.
+          <br/>
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          $250+ | 240 minutes
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Book</Button>
+      </CardActions>
+    </Card>
+
+    <br/>
+
+    <Card sx={{ maxWidth: 345 }}>
+      <CardMedia
+        sx={{ height: 140 }}
+        image="/static/images/cards/contemplative-reptile.jpg"
+        title="green iguana"
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+          Annual Brow Touch Up/Color Boost
+          Color touch up for previously shaded brows (permanent makeup)
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          $100+ | 180 minutes
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small">Book</Button>
+      </CardActions>
+    </Card>
+
             </div>
         )
     }
